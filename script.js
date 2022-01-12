0309dc7401e42bd5f66ad688fb50e1f4d6ec6749
KuhakuPixel <Nicholaspixels@gmail.com>
2022-01-12 20:19:48 +0700

    Add modal when pressing submit button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var learnmore = document.getElementById("btn-learnmore")
 var email = document.getElementById("email").value
-var submit = document.getElementById("btn-submit")
+var submitBtn = document.getElementById("btn-submit")
+$('#myModal').modal({ show: false });
 
-learnmore.addEventListener("click", ()=>{
+//trigger when form is submitted
+$("#email-submit-form").submit(function (e) {
+  console.log("Submitted");
+  $('#myModal').modal('show');
+
+  return false;
+});
+learnmore.addEventListener("click", () => {
   alert("Learn more isn't paired yet!")
 })
 
-submit.addEventListener("click", ()=>{
-  alert("Your email has been registered.")
+submitBtn.addEventListener("click", () => {
+  //$('#myModal').modal('show');
+
 })
