4aebc780aae48cdd57d240624ea5b1a8c008e567
NicholasAndreas <>
2022-01-12 12:45:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var learnmore = document.getElementById("btn-learnmore")
+var email = document.getElementById("email").value
+var submit = document.getElementById("btn-submit")
+
+learnmore.addEventListener("click", ()=>{
+  alert("Learn more isn't paired yet!")
+})
+
+submit.addEventListener("click", ()=>{
+  alert("Your email has been registered.")
+})
