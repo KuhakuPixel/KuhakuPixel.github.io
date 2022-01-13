cf3079a383808d7e5208898d7b2560a051669500
KuhakuPixel <Nicholaspixels@gmail.com>
2022-01-13 21:00:36 +0700

    add onsubmit button for form

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var learnmore = document.getElementById("btn-learnmore")
 var email = document.getElementById("email").value
 var submit = document.getElementById("btn-submit")
-
-learnmore.addEventListener("click", ()=>{
+var emailSubmitForm = document.getElementById("email-form");
+learnmore.addEventListener("click", () => {
   alert("Learn more isn't paired yet!")
 })
 
-submit.addEventListener("click", ()=>{
-  alert("Your email has been registered.")
+submit.addEventListener("click", () => {
+  //alert("Your email has been registered.")
 })
+emailSubmitForm.addEventListener("submit", () => {
+  alert("Your  has been registered.");
+});
