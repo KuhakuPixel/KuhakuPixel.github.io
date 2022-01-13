a1902ded94278641521a4366e160a6dfef589a93
KuhakuPixel <Nicholaspixels@gmail.com>
2022-01-13 15:39:16 +0700

    Revision back to when navbar is not broken

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
 var learnmore = document.getElementById("btn-learnmore")
 var email = document.getElementById("email").value
-var submitBtn = document.getElementById("btn-submit")
+var submit = document.getElementById("btn-submit")
 
-// get all register buttons by class name
-
-var registerNowButtons = document.getElementsByClassName("register-button");
-
-//trigger when form is submitted
-$("#email-submit-form").submit(function (e) {
-  console.log("Submitted");
-  $('#emailSubmittedModal').modal('show');
-
-  return false;
-});
-
-//trigger when registring for workshop
-$("#workshop-register-form").submit(function (e) {
-  console.log("register");
-  //$('#registerModal').modal('show');
-
-  return false;
-});
-//register events for all registernow button
-for (var i = 0; i < registerNowButtons.length; i++) {
-  registerNowButtons[i].addEventListener("click", () => {
-    alert("opening modal");
-    $('#exampleModal').modal('show');
-  });
-
-}
-learnmore.addEventListener("click", () => {
+learnmore.addEventListener("click", ()=>{
   alert("Learn more isn't paired yet!")
-});
+})
+
+submit.addEventListener("click", ()=>{
+  alert("Your email has been registered.")
+})
